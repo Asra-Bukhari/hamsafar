86ef20b654ec478377679221a659b1ab2cb7d67d
Asra-Bukhari <asrabukhari2003@gmail.com>
2025-07-13 06:10:17 -0700

    Add Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import Login from "./components/LoginPage";
+import FeedPage from "./components/FeedPage";
+import ProtectedRoute from "./components/ProtectedRoutes";
+import MyProfilePage from "./components/ProfilePage";
+import SignUp from "./components/SignupPage";
+import LogoPage from "./components/LogoPage";
+import HomePage from "./components/homepage";
+import NotificationListener from "./components/notificationlistener";
+import DriverRegistration from "./components/DriverRegistration";
+
+import ScheduleTrip from "./components/scheduletrip";
+import JoinTrip from "./components/jointrip";
+import AllVehiclesPage from "./components/Allvehicles";
+import AllUsersPage from "./components/AllUsers";
+import AllScheduledTripsPage from "./components/AllScheduledTrips";
+import Recenttrips from "./components/TripsHistoryPage";
+import RatingsOverviewPage from "./components/RatingsPage";
+import Friendgroups from "./components/FriendsGroupsPage";
+import Updateprofile from "./components/EditProfilePage";
+import SettingsPage from "./components/ProfileSettings";
+import RatingsPage from "./components/RatingsPage2";
+import TripInfo from "./components/tripinfo"; // <--- added TripInfo import
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <ToastContainer position="bottom-right" autoClose={4000} theme="colored" />
+        <NotificationListener />
+
+        <Routes>
+          <Route path="/" element={<LogoPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<ProtectedRoute><FeedPage /></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path="/my-profile" element={<MyProfilePage />} />
+          <Route path="/register-driver" element={<DriverRegistration />} />
+        
+          <Route path="/alltrips" element={<AllScheduledTripsPage />} />
+          <Route path="/view-all-vehicles" element={<AllVehiclesPage />} />
+          <Route path="/allusers" element={<AllUsersPage />} />
+          <Route path="/Settings-see" element={<SettingsPage />} />
+          <Route path="/my-ratings" element={<RatingsOverviewPage />} />
+          <Route path="/my-trips" element={<Recenttrips />} />
+          <Route path="/my-FriendGroups" element={<Friendgroups />} />
+          <Route path="/Edit-My-Profile" element={<Updateprofile />} />
+          <Route path="/rate/:tripId/:driverId" element={<RatingsPage />} />
+          <Route path="/scheduletrip" element={<ScheduleTrip />} />
+          <Route path="/jointrip" element={<JoinTrip />} />
+          <Route path="/tripinfo" element={<TripInfo />} /> {/* <-- added TripInfo route */}
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
